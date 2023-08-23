c122eb67acb8564fa12da63c9839c818e3769a8d
EC2 Default User <ec2-user@ip-172-31-46-53.ap-northeast-1.compute.internal>
2023-08-23 14:49:52 +0000

    課題3[NoMethodError]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
   def index
     @users = User.all
     @book = Book.new
+    @user = current_user
   end
   
   def create
@@ -12,6 +13,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @users = @user.books
+    @book_new = Book.new
   end
   
   def edit
