90053c0fa5b3775d9feb7cca5005ae8e579990f2
EC2 Default User <ec2-user@ip-172-31-46-53.ap-northeast-1.compute.internal>
2023-08-28 14:39:43 +0000

    課題3[バリデーション中]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -35,8 +35,12 @@ class BooksController < ApplicationController
   
   def update
     book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book)
+    if book.update(book_params)
+      redirect_to book_path(book)
+    else
+      @book = Book.find(params[:id])
+      render :edit
+    end
   end
   
   def destroy
