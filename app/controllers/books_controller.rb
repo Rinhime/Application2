1b916fa933e87a1dd2e3563e3596e59b23181c95
EC2 Default User <ec2-user@ip-172-31-46-53.ap-northeast-1.compute.internal>
2023-09-02 07:42:11 +0000

    book edit リンク完成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -14,6 +14,7 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
+      flash[:notice] ="You have created book successfully."
       redirect_to book_path(@book)
     else
       @books =Book.all
@@ -35,6 +36,7 @@ class BooksController < ApplicationController
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
+      flash[:notice] ="You have updated book successfully."
       redirect_to book_path(book)
     else
       render :edit
@@ -55,7 +57,7 @@ class BooksController < ApplicationController
    
    def is_matching_login_user
      book = Book.find(params[:id])
-     unless book.id == current_user.id
+     unless book.user_id == current_user.id
        redirect_to books_path
      end
    end
